9b1455ffc46188942a71b009d15ebbb1b65eea0a
hsh-newbi <hsonghart0426@gmail.com>
2023-11-02 21:41:53 +0900

    자바스크립트 강의 - 이벤트 버블링과 숨겨진 데이터를 활용한 탭 기능 만들기

--- 231026/tab.js
+++ 231026/tab.js
@@ -19,11 +19,25 @@ const tabContents = $(".tab-content");
 
 // 애플코딩 답 코드
 
-for (let i = 0; i < btns.length; i++) {
-  btns.eq(i).on("click", function () {
-    btns.removeClass("orange");
-    btns.eq(i).addClass("orange");
-    tabContents.removeClass("show");
-    tabContents.eq(i).addClass("show");
-  });
+// for (let i = 0; i < btns.length; i++) {
+//   btns.eq(i).on("click", function () {
+//     btns.removeClass("orange");
+//     btns.eq(i).addClass("orange");
+//     tabContents.removeClass("show");
+//     tabContents.eq(i).addClass("show");
+//   });
+// }
+
+// data-set과 이벤트 버블링을 활용한 방법
+
+function activeTab(num) {
+  btns.removeClass("orange");
+  btns.eq(num).addClass("orange");
+  tabContents.removeClass("show");
+  tabContents.eq(num).addClass("show");
 }
+
+$(".list").click(function (e) {
+  const currentID = parseInt(e.target.dataset.id);
+  activeTab(currentID);
+});
