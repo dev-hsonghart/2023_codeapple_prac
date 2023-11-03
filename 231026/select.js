1d46bdf5387d3374115c758b1a33cdb863a61e39
hsh-newbi <hsonghart0426@gmail.com>
2023-11-03 13:57:57 +0900

    자바스크립트 숙제 21 - 애플코딩 해설로 구현해보기 : 이벤트리스터의 Input을 이용

--- 231026/select.js
+++ 231026/select.js
@@ -1,10 +1,20 @@
 const selectProduct = document.querySelector(".select-1");
 const selectSize = document.querySelector(".select-2");
 
-const changeFn = function (target) {
-  if (target.value == "shirt") {
+// const changeFn = function (target) {
+//   if (target.value == "shirt") {
+//     selectSize.classList.add("show");
+//   } else {
+//     selectSize.classList.remove("show");
+//   }
+// };
+
+selectProduct.addEventListener("input", function (e) {
+  const value = e.currentTarget.value;
+  console.log(value);
+  if (value == "셔츠") {
     selectSize.classList.add("show");
   } else {
     selectSize.classList.remove("show");
   }
-};
+});
