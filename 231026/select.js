161fa95569ec14b101b08874358fc9427f2afd89
hsh-newbi <hsonghart0426@gmail.com>
2023-11-03 14:10:52 +0900

    자바스크립트 숙제 22 - 자바스크립트로 Html 요소 추가하기 : innerHTMl / insertAdjacentHTML 응용

--- 231026/select.js
+++ 231026/select.js
@@ -11,9 +11,16 @@ const selectSize = document.querySelector(".select-2");
 
 selectProduct.addEventListener("input", function (e) {
   const value = e.currentTarget.value;
-  console.log(value);
+
   if (value == "셔츠") {
     selectSize.classList.add("show");
+    const option = `<option>90</option>
+    <option>100</option>`;
+    selectSize.innerHTML = option;
+  } else if (value == "바지") {
+    const option = `<option>25</option><option>30</option>`;
+
+    selectSize.innerHTML = option;
   } else {
     selectSize.classList.remove("show");
   }
