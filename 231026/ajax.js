517963519817e12d335edefd8b76ddbccf958213
hsh-newbi <hsonghart0426@gmail.com>
2023-11-06 13:13:05 +0900

    자바스크립트 숙제 23 - 코드 다듬기

--- 231026/ajax.js
+++ 231026/ajax.js
@@ -1,11 +1,19 @@
+var products = [
+  { id: 0, price: 70000, title: "Blossom Dress" },
+  { id: 1, price: 50000, title: "Springfield Shirt" },
+  { id: 2, price: 60000, title: "Black Monastery" },
+];
+
 const row = document.querySelector(".row");
 
-const cardTemplete = `<div class="col-sm-4">
-<img src="https://via.placeholder.com/600" class="w-100" />
-<h5>Card title</h5>
-<p>가격 : 70000</p>
-</div>`;
+const cardTemplete = function (index) {
+  return `<div class="col-sm-4">
+  <img src="https://via.placeholder.com/600" class="w-100" />
+  <h5>${products[index].title}</h5>
+  <p>가격 : ${products[index].price}</p>
+  </div>`;
+};
 
-for (let i = 0; i < 3; i++) {
-  row.insertAdjacentHTML("beforeend", cardTemplete);
+for (let i = 0; i < products.length; i++) {
+  row.insertAdjacentHTML("beforeend", cardTemplete(i));
 }
