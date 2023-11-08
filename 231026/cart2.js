ca992ae1298a1ad4accf3361a60a05130816a9a6
hsh-newbi <hsonghart0426@gmail.com>
2023-11-08 23:57:11 +0900

    자바스크립트 기능 예제 - 4번 구현 완료

--- 231026/cart2.js
+++ 231026/cart2.js
@@ -1,13 +1,4 @@
-let cartStoreList = [
-  {
-    id: 0,
-    title: "이미 있음",
-    brand: "이미 있는것",
-    photo: "./",
-    price: "10000",
-    count: 1,
-  },
-];
+let cartStoreList = [];
 const cartRow = document.querySelector("#cartRow");
 
 const cartTemplete = function (data, i) {
@@ -17,34 +8,38 @@ const cartTemplete = function (data, i) {
     <h5>${data[i].title}</h5>
     <p>${data[i].brand}</p>
     <p>가격 : ${data[i].price}</p>
-    <input type="text" value="${data[i].count}" />
+    <input type="number" value="${data[i].count}" id="test"/>
   </div>
 </div>`;
 };
 
+// 수량 입력칸 클릭 시 수량 반영하기
+function clickCount() {
+  const inputs = $("#test");
+  inputs.blur(function (e) {
+    const parentId = e.target.parentNode.id;
+    console.log("수량조절완료");
+    const fixedCount = e.target.value;
+    console.log(parentId);
+    // 수량조절 완료 후 input value 값이 1 이상일 경우 해당 obj에 수량 반영
+    if (fixedCount >= 1) {
+      console.log("수량 반영해야함");
+    } else {
+      // input value 값이 0 이하일 경우 alert 출력 후 1로 복귀
+      alert("0개 이상 작성해야함");
+    }
+  });
+}
+
 // 담기 버튼 클릭 이벤트 발생 위치 찾기
 
-// 장바구니 배열의 데이터 장바구니 영역에 출력하기
-
 function makeCartProduct(array) {
+  // 장바구니 배열의 데이터를 장바구니 영역에 출력하기
   cartRow.innerHTML = "";
   for (let i = 0; i < array.length; i++) {
     // 템플릿에 하나씩 저장하고 html 에 출력하기
     cartRow.insertAdjacentHTML("beforeend", cartTemplete(array, i));
   }
-}
-
-function makeNewItem(item) {
-  // 장바구니용 객체 생성해서 리턴
-  const newItem = {
-    id: item.id,
-    title: item.title,
-    brand: item.brand,
-    photo: item.photo,
-    price: item.price,
-    count: 1,
-  };
-  return newItem;
 }
 
 function findProduct(objId) {
@@ -57,34 +52,53 @@ function findProduct(objId) {
     }
   }
   const item = baseObj(objId);
-  makeNewItem(item);
+  // 기존 obj를 새로운 객체에 복사하기
+  const newItem = {
+    id: item.id,
+    title: item.title,
+    brand: item.brand,
+    photo: item.photo,
+    price: item.price,
+    count: 1,
+  };
+  return newItem;
 }
 
-// 장바구니에 아이템 없음
+// 장바구니에 아이템 없으로 기초값 설정
 let isCart = false;
 
 function cartInProduct(e) {
   // 버튼의 부모 div의 id를 가져온다
   const parentId = e.target.parentNode.id;
-  console.log(parentId);
-  // div id와 동일한 id를 가진 obj를 storeList에서 찾는다.
 
-  if (isCart == true) {
-    console.log("장바구니에 아이템이 있음");
-    // 클릭한 아이템의 아이디가 장바구니에 없으면
-
-    // 새로운 객체 생성해서 기존 객체 데이터 복사하기
+  function findItem(item) {
+    // 클릭한 버튼의 id와 같은 id를 가진 아이템이 있는지 확인
+    // 있으면 객체를 반환하고 없으면 undefined 반환
+    return item.id == parentId;
   }
+  // div id와 동일한 id를 가진 obj를 storeList에서 찾는다.
+  if (isCart == false) {
+    const newProduct = findProduct(parentId);
+    cartStoreList.push(newProduct);
+    isCart = !isCart;
+  } else {
+    // 클릭한 id와 동일한 아이템 있는지 확인하고 있으면 아이템 꺼내오기
+    const isInProduct = cartStoreList.find(findItem);
+    if (isInProduct != undefined) {
+      // 장바구니에 해당 아이템이 이미 있음
+      isInProduct.count += 1;
+    } else {
+      // 장바구니에 해당 아이템은 아직 없어서 새로 추가함
 
-  // cartView에 출력하기
-
-  // makeCartProduct(cartStoreList);
+      const newProduct = findProduct(parentId);
+      cartStoreList.push(newProduct);
+    }
+  }
+  makeCartProduct(cartStoreList);
+  clickCount();
 }
 
 window.onload = function () {
-  const btns = document.querySelectorAll(".buy");
-  btns[0].addEventListener("click", cartInProduct);
-  btns[1].addEventListener("click", cartInProduct);
-  // btns[2].addEventListener("click", cartInProduct);
-  // btns[3].addEventListener("click", cartInProduct);
+  const btns = $(".buy");
+  btns.on("click", cartInProduct);
 };
